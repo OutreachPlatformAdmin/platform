fdd238d2d032e73e243e37e4ab8ffadae6b731da
Antonio <antoniosansotta@gmail.com>
2023-04-06 21:38:07 -0400

    added configuration for sveltekit fonts, tailwindcss, and picocss. Began adding the layout and main pages.

---
+++ frontend/src/routes/+layout.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+	import { Menu } from 'lucide-svelte';
+	import '../app.css';
+</script>
+
+<nav>
+	<ul>
+		<li>
+			<a href="#" class="secondary"><Menu /></a>
+		</li>
+	</ul>
+	<ul>
+		<li class="text-3xl"><strong>Platform</strong></li>
+	</ul>
+	<ul>
+		<li><a href="#" class="secondary">Feedback</a></li>
+	</ul>
+</nav>
+
+<slot />
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,51 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<header>
+	<figure class="relative">
+		<img
+			class="object-cover w-full h-[400px]"
+			src="/images/banner-image.jpg"
+			alt="banner of a hand and globe"
+		/>
+		<figcaption class="absolute right-0 bottom-0 z-10 opacity-75 mr-5">
+			Photo by <a
+				href="https://unsplash.com/@weirick?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+				class="text-cyan-600">Jake Weirick</a
+			>
+			on
+			<a
+				href="https://unsplash.com/photos/C47_TXZZwIM?utm_source=unsplash&utm_medium=referral&utm_content=creditCopyText"
+				class="text-cyan-500">Unsplash</a
+			>
+		</figcaption>
+	</figure>
+
+	<h1 class="w-full mt-5 pt-3 pb-5 text-5xl border-t-4 border-b-4 border-slate-500 text-center">
+		Welcome
+	</h1>
+	<section class="">
+		<div>
+			<p class="my-5 indent-10">
+				Welcome to a platform for learning how the world works, what can be done about it, and how
+				you can get involved. Check out the intro video to learn why we bothered creating this
+				space.
+			</p>
+			<p class="indent-10">
+				If you're not sure where to start, check out the guided learning path for a structured
+				school-like learning experience, or ask some common questions in the questions section.
+				Either way, browse around and learn how we can change the world for the better.
+			</p>
+		</div>
+		<!-- svelte-ignore a11y-media-has-caption -->
+		<div class="w-full ml-1/2 mr-auto">
+			<iframe
+				class="w-full aspect-video max-w-lg"
+				src="https://www.youtube.com/embed/uWSxzjyMNpU"
+				title="YouTube video player"
+				frameborder="0"
+				allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+				allowfullscreen
+			/>
+		</div>
+	</section>
+
+	<iframe src="https://mkorostoff.github.io/1-pixel-wealth/" height="600" class="w-full my-10" />
+</header>
