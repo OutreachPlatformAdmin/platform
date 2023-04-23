470dd82238af0b7557244792f4d6d10694e7b852
John Desmond <dovyalis.hebecarpa@gmail.com>
2023-04-23 15:45:31 -0400

    added in a single POST endpoint to create a new topic

--- backend/crm_api/src/routes/topics.rs
+++ backend/crm_api/src/routes/topics.rs
@@ -1,10 +1,20 @@
-use axum::{extract::State, Json};
+use axum::{
+    extract::State,
+    http::StatusCode,
+    response::{IntoResponse, Response},
+    Json,
+};
 use serde::{Deserialize, Serialize};
-use sqlx::{FromRow, PgPool, Result};
+use sqlx::{query, FromRow, PgPool, Result};
 
 #[derive(Serialize, Deserialize, FromRow)]
 pub struct Topic {
     id: i32,
+    topic: String,
+}
+
+#[derive(Deserialize)]
+pub struct CreateTopic {
     topic: String,
 }
 
@@ -19,4 +29,19 @@ pub async fn get_all_topics(db_pool: &PgPool) -> Result<Vec<Topic>> {
         .await?;
 
     Ok(topics)
+}
+
+pub async fn new_topic_handler(
+    State(db_pool): State<PgPool>,
+    Json(payload): Json<CreateTopic>,
+) -> Response {
+    let topic = &payload.topic;
+    let insert_result = query!("INSERT INTO platform.topics (topic) VALUES ($1)", topic)
+        .execute(&db_pool)
+        .await;
+    let insert = match insert_result {
+        Ok(_insert_result) => "new topic created".into_response(),
+        Err(_err) => (StatusCode::INTERNAL_SERVER_ERROR, "Insertion to DB failed").into_response(),
+    };
+    insert
 }
